9792fb31fba4c2eb4f4a9a321b8b7e3506f98b3e
needlzzz3 <needlzzz@mail.ru>
2020-06-17 21:04:10 +0300

    тест успешно завершился

--- test_add_group.py
+++ test_add_group.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
+import selenium.common.exceptions
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
