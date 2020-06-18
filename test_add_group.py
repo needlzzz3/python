91fd854f7a1a698eef2f7b23413a45e38918a308
needlzzz3 <needlzzz@mail.ru>
2020-06-18 22:25:14 +0300

    произведен рефакторинг, строки объеденены в методы, упорядочены в логической последовательности

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoAlertPresentException, NoSuchElementException
 import unittest
 
 class AddGroup(unittest.TestCase):
@@ -8,60 +8,56 @@ class AddGroup(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd)
         self.open_group_page(wd)
-        self.create_new_group(wd)
-        self.back_to_group_page(wd)
+        self.create_group(wd)
+        self.retern_group_page(wd)
         self.logout(wd)
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-        # разлогинились
-
-    def back_to_group_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-        # вернулись на страницу с группами
-
-    def create_new_group(self, wd):
-        wd.find_element_by_name("new").click()
-        # создаем новую группу
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("5555")
-        # ввели имя группы
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("5555")
-        # ввели заголовок
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("5555")
-        # ввели данные
-        wd.find_element_by_name("submit").click()
-        # подтвердили создание группы
-
-    def open_group_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
-        # переходим в раздел групп
+    def open_home_page(self, wd):
+        # open main page
+        wd.get("http://localhost/addressbook/group.php")
 
     def login(self, wd):
-        # залогиниться
+        # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
+        # push login button
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        # нажимаем кнопку логина
 
-    def open_home_page(self, wd):
-        # открыть главную страницу
-        wd.get("http://localhost/addressbook/group.php")
+    def open_group_page(self, wd):
+        # go to group page
+        wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd):
+        # create group
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("5555")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("5555")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("5555")
+        # push submit button
+        wd.find_element_by_name("submit").click()
+
+    def retern_group_page(self, wd):
+        # retern group page
+        wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
+        # logout
+        wd.find_element_by_link_text("Logout").click()
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
