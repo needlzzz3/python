79ecaa43ee3f7316658969bc10d6fc71c069b2e5
needlzzz3 <needlzzz@mail.ru>
2020-06-20 18:29:05 +0300

    доавлен класс

--- group.py
+++ group.py
@@ -3,3 +3,22 @@ class Group:
             self.groupname = groupname
             self.header = header
             self.footer = footer
+class Contact
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work,
+                    fax, email, email2, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname =nickname
+        self.title= title
+        self.company= company
+        self.address=address
+        self.home=home
+        self.mobile =mobile
+        setf.work=work
+        self.fax=fax
+        self.email=email
+        self.email2-=email2
+        self.address2=address2
+        self.selfphone2=phone2
+        self.notes=notes
--- test_add_contact.py
+++ test_add_contact.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.common.exceptions import NoAlertPresentException, NoSuchElementException
 import unittest
 
+from group import Contact
+
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
@@ -13,7 +15,19 @@ class TestAddContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.add_contact(wd)
+        self.add_contact(wd, Contact (firstname="F", middlename="F", lastname="F", nickname="FFF", title="FFF", company= "none", address="none", home="adress", mobile="none",
+                         work="none", fax="none", email= "none", email2="none", address2="none", phone2="none", notes="none contact"))
+        self.retern_group_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.add_contact(wd, Contact (firstname="a", middlename="a", lastname="a", nickname="", title="", company="",
+                         address="", home="", mobile="",
+                         work="", fax="", email="", email2="", address2="", phone2="",
+                         notes="empty contact"))
         self.retern_group_page(wd)
         self.logout(wd)
 
@@ -25,48 +39,48 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_link_text("home page").click()
         # вернулись на главную
 
-    def add_contact(self, wd):
+    def add_contact(self, wd, Contact):
         wd.find_element_by_link_text("add new").click()
         # открыли станицу для добавления контактов
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("F")
+        wd.find_element_by_name("firstname").send_keys(Contact.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("F")
+        wd.find_element_by_name("middlename").send_keys(Contact.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("F")
+        wd.find_element_by_name("lastname").send_keys(Contact.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("FFF")
+        wd.find_element_by_name("nickname").send_keys(Contact.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("FFF")
+        wd.find_element_by_name("title").send_keys(Contact.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("none")
+        wd.find_element_by_name("company").send_keys(Contact.company)
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("none")
+        wd.find_element_by_name("address").send_keys(Contact.address)
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("adress")
+        wd.find_element_by_name("home").send_keys(Contact.home)
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("none")
+        wd.find_element_by_name("mobile").send_keys(Contact.mobile)
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("none")
+        wd.find_element_by_name("work").send_keys(Contact.work)
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys("none")
+        wd.find_element_by_name("fax").send_keys(Contact.fax)
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("none")
+        wd.find_element_by_name("email").send_keys(Contact.email)
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys("none")
+        wd.find_element_by_name("email2").send_keys(Contact.email2)
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("none")
+        wd.find_element_by_name("address2").send_keys(Contact.address2)
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("none")
+        wd.find_element_by_name("phone2").send_keys(Contact.phone2)
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("none contact")
+        wd.find_element_by_name("notes").send_keys(Contact.notes)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         # добавили контакт
 
