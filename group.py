22e913ed54e39593ae0c9fc35a6ef441c94c5586
needlzzz3 <needlzzz@mail.ru>
2020-06-20 20:19:15 +0300

    исправлены ошибки в классах

--- group.py
+++ group.py
@@ -1,24 +1,28 @@
 class Group:
-        def __init__(self, groupname, header, footer):
-            self.groupname = groupname
-            self.header = header
-            self.footer = footer
-class Contact
+
+    def __init__(self, groupname, header, footer):
+        self.groupname = groupname
+        self.header = header
+        self.footer = footer
+
+
+class Contact:
+
     def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work,
-                    fax, email, email2, address2, phone2, notes):
+                 fax, email, email2, address2, phone2, notes):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
-        self.nickname =nickname
-        self.title= title
-        self.company= company
-        self.address=address
-        self.home=home
-        self.mobile =mobile
-        setf.work=work
-        self.fax=fax
-        self.email=email
-        self.email2-=email2
-        self.address2=address2
-        self.selfphone2=phone2
-        self.notes=notes
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
