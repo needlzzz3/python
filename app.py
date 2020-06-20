fcbceb0c7ad52fdc049838c729b22fce864b82cc
needlzzz3 <needlzzz@mail.ru>
2020-06-20 21:09:07 +0300

    save

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 
 class App:
+
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from group import Group
-import App from app
 import pytest
+from group import Group
+from app import App
 
 
 @pytest.fixture
@@ -15,6 +15,7 @@ def test_add_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(groupname="5555", header="5555", footer="5555"))
     app.logout()
+
 
 def test_add_empty_group(app):
     app.login(username="admin", password="secret")
