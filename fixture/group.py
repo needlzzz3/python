4dd14620338b8aee7c5526dea32a3e2215e75e41
needlzzz3 <needlzzz@mail.ru>
2020-07-01 08:22:50 +0300

    создала тест по модификации группы

--- fixture/group.py
+++ fixture/group.py
@@ -35,6 +35,24 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.retern_group_page()
 
+    def modificate_group(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("(//input[@name='edit'])[2])").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.groupname)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # push update button
+        wd.find_element_by_name("update").click()
+        self.retern_group_page()
+
 
     def retern_group_page(self):
         # retern group page
