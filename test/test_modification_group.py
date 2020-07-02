c42a48edea08444d838a29e7458f6828db852503
needlzzz3 <needlzzz@mail.ru>
2020-07-02 10:26:03 +0300

    upgrade group tests

--- test/test_modification_group.py
+++ test/test_modification_group.py
@@ -3,9 +3,13 @@ import pytest
 from model.group import Group
 
 def test_modificate_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(groupname="test"))
     app.group.modificate_first_group(Group(groupname="666"))
 
 def test_modificate_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(groupname="test"))
     app.group.modificate_first_group(Group(header="666"))
 
 
