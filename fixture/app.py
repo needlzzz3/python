cabafc0fc5713c89f43e0c09b27efcba8c55483f
needlzzz3 <needlzzz@mail.ru>
2020-07-23 12:44:03 +0300

    16 задание

--- fixture/app.py
+++ fixture/app.py
@@ -6,16 +6,24 @@ from fixture.contact import ContactHelper
 
 class App:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def open_home_page(self):
         # open main page
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get(self.base_url)
 
     def is_valid(self):
         try:
