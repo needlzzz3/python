896961e81cc392450009314dcc78a30ea0ecf3c5
needlzzz3 <needlzzz@mail.ru>
2020-07-26 19:24:05 +0300

    урок 7 Загрузка информации из базы данных в виде объектов

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ import json
 import os.path
 import jsonpickle
 import importlib
+from fixture.db import DbFixture
 
 
 fixture = None
@@ -22,18 +23,24 @@ def load_config(file):
 @pytest.fixture
 def app(request):
     global fixture
-    global target
     browser = request.config.getoption("--browser")
-    if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-        with open(config_file) as f:
-            target = json.load(f)
+    web_config = load_config(request.config.getoption("--target"))["web"]
     if fixture is None or not fixture.is_valid():
-        fixture = App(browser=browser, base_url=target["baseUrl"])
-    fixture.session.ensure_login(username=target["username"], password=target["password"])
+        fixture = App(browser=browser, base_url=web_config["baseUrl"])
+    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
 
-@pytest.fixture(scope = "session", autouse = True)
+@pytest.fixture(scope = "session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))["db"]
+    dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
+
+
+@pytest.fixture(scope="session", autouse = True)
 def stop(request):
     def fin():
         fixture.session.ensure_logout()
