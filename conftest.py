d77eab6f5c0b8a70f77eb47d27daf595adaa200b
needlzzz3 <needlzzz@mail.ru>
2020-08-11 17:37:07 +0300

    unit 10

--- conftest.py
+++ conftest.py
@@ -70,7 +70,7 @@ def load_from_module(module):
 
 
 def load_from_json(file):
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data\%s.json" % file)) as f:
         return jsonpickle.decode(f.read())
 
 @pytest.fixture
