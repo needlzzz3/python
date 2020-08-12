ab47ecfbad967ca5127d5ce9d8324c4f503f4f1e
needlzzz3 <needlzzz@mail.ru>
2020-08-12 07:18:58 +0300

    ex 22

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -7,21 +7,18 @@ import random
 
 
 
-db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+orm = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 
 
 def test_add_contact_to_group(app, db):
     groups = db.get_group_list()
+    contacts = db.get_contact_list()
     if len(groups) == 0:
         app.group.create(Group(name="test"))
-        new_groups = db.get_group_list()
-    group = random.choice(groups)
-    contacts = db.contacts_without_groups()
     if len(contacts) == 0:
         app.contact.contact(Contact(firstname="test"))
-        new_contacts = db.contacts_without_groups()
-    contact = random.choice(contacts)
-    app.contact.add_contact_to_group(contact, group)
-    assert len(contacts) == len(new_contacts) + 1
-    assert len(groups) == len(new_groups) + 1
+    contacts_without_groups = orm.contacts_without_groups()
+    app.contact.add_contact_to_group()
+    new_contacts_without_groups = orm.contacts_without_groups()
+    assert len(contacts_without_groups)-1 == len(new_contacts_without_groups)
