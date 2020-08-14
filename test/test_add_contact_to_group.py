af72ed4498e12703297df86761ea135e6288d844
needlzzz3 <needlzzz@mail.ru>
2020-08-14 16:12:16 +0300

    ex22

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -13,12 +13,15 @@ orm = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 def test_add_contact_to_group(app, db):
     groups = db.get_group_list()
-    contacts = db.get_contact_list()
+    contacts = db.get_group_list()
     if len(groups) == 0:
         app.group.create(Group(name="test"))
     if len(contacts) == 0:
         app.contact.contact(Contact(firstname="test"))
-    contacts_without_groups = orm.contacts_without_groups()
-    app.contact.add_contact_to_group()
-    new_contacts_without_groups = orm.contacts_without_groups()
-    assert len(contacts_without_groups)-1 == len(new_contacts_without_groups)
+    free_groups = orm.get_group_list()
+    free_contacts = orm.get_group_list()
+    app.contact.add_contact_to_group(free_groups[0], free_contacts[0])
+    new_free_groups = orm.get_group_list()
+    new_free_contacts = orm.get_group_list()
+    assert len(free_contacts) == len(new_free_contacts) - 1
+    assert len(free_groups) == len(new_free_groups) - 1
--- test/test_del_contact_from_group.py
+++ test/test_del_contact_from_group.py
@@ -9,14 +9,16 @@ orm = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 def test_del_contact_from_group(app, db):
     groups = db.get_group_list()
-    contacts = db.get_contact_list()
+    contacts = ddb.get_group_list()
     if len(groups) == 0:
         app.group.create(Group(name="test"))
     if len(contacts) == 0:
         app.contact.contact(Contact(firstname="test"))
+    free_groups = db.get_group_list()
+    free_contacts = db.get_group_list()
     contacts_in_groups = orm.contacts_in_groups()
     if len(contacts_in_groups) == 0:
-        app.contact.add_contact_to_group()
+        app.contact.add_contact_to_group(free_groups, free_contacts)
         contacts_in_groups = orm.contacts_in_groups()
     contact_n_group = contacts_in_groups[0]
     group_n_contact = orm.get_groups_of_contact(contact_n_group)[0]
